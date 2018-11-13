aec4dbc4fc172bf68ba3a3377a9bef1415552a9c
Jonathan Bian <biantech@163.com>
2018-11-13 15:30:38 +0800

    create user table
    create user table

---
+++ spring-mybatis-01/src/main/sql/create-table.sql
@@ -0,0 +1,11 @@
+create table user(
+	login_id varchar(30),
+	name     varchar(30) ,
+	age  int,
+	sex  varchar(20),
+	duty varchar(30),
+	cell_number varchar(30),
+	photo_url varchar(130),
+	pwd varchar(30),
+	used bit	
+);
