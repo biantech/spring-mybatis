da1f582154a30b2e5c5c7e2d397f47df35567053
Jonathan Bian <biantech@163.com>
2018-11-14 19:31:41 +0800

    Update UserServiceTest.java

--- spring-mybatis-01/src/test/java/com/biantech/ssmd/test/service/UserServiceTest.java
+++ spring-mybatis-01/src/test/java/com/biantech/ssmd/test/service/UserServiceTest.java
@@ -5,12 +5,15 @@ import com.biantech.ssmd.service.serviceImpl.UserServiceImpl;
 import com.biantech.ssmd.test.BaseTest;
 import com.biantech.ssmd.exception.OtherThingsException;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Administrator on 2016/9/25.
  */
 public class UserServiceTest extends BaseTest {
+    Logger logger = LoggerFactory.getLogger(UserServiceTest.class);
     @Autowired
     private UserServiceImpl userService;
 
@@ -23,7 +26,13 @@ public class UserServiceTest extends BaseTest {
             //其他综合异常或是不能处理的异常
             e.printStackTrace();
         }
+    }
 
-
+    @Test
+    public void testSelect() {
+        User user = new User();
+        user.setLoginId("111111");
+        user=userService.findUser(user);
+        logger.info("user="+user);
     }
 }
