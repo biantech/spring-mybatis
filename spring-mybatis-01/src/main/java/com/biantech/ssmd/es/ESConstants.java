0f0b555783656ff3ec3677081c6b7906426d413e
Jonathan Bian <biantech@163.com>
2018-11-14 19:07:07 +0800

    Create ESConstants.java

---
+++ spring-mybatis-01/src/main/java/com/biantech/ssmd/es/ESConstants.java
@@ -0,0 +1,30 @@
+/**
+ * @author
+ * @date 2018/11/13
+ */
+public class ESConstants {
+    public static final String FIELD_DATA = "fielddata";
+    public static final String FIELD_STORE = "store";
+    public static final String FIELD_TYPE = "type";
+    public static final String FIELD_INDEX = "index";
+    public static final String FIELD_FORMAT = "format";
+    public static final String FIELD_SEARCH_ANALYZER = "search_analyzer";
+    public static final String FIELD_INDEX_ANALYZER = "analyzer";
+    public static final String FIELD_PROPERTIES = "properties";
+    public static final String FIELD_PARENT = "_parent";
+    public static final String FIELD_TYPE_INT = "integer";
+    public static final String FIELD_TYPE_LONG = "long";
+    public static final String FIELD_TYPE_SHORT = "short";
+    public static final String FIELD_TYPE_BYTE = "byte";
+    public static final String FIELD_TYPE_DOUBLE = "double";
+    public static final String FIELD_TYPE_FLOAT = "float";
+    public static final String COMPLETION_PRESERVE_SEPARATORS = "preserve_separators";
+    public static final String COMPLETION_PRESERVE_POSITION_INCREMENTS = "preserve_position_increments";
+    public static final String COMPLETION_MAX_INPUT_LENGTH = "max_input_length";
+
+    public static final String TYPE_VALUE_KEYWORD = "keyword";
+    public static final String TYPE_VALUE_GEO_POINT = "geo_point";
+    public static final String TYPE_VALUE_COMPLETION = "completion";
+    public static final String TYPE_VALUE_GEO_HASH_PREFIX = "geohash_prefix";
+    public static final String TYPE_VALUE_GEO_HASH_PRECISION = "geohash_precision";
+}
