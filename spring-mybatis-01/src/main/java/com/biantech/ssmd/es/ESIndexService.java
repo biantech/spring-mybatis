d2a1385f05e5ac482e4f00dcb3765f9c0de5b71a
Jonathan Bian <biantech@163.com>
2018-12-25 15:20:46 +0800

    Update ESIndexService.java

--- spring-mybatis-01/src/main/java/com/biantech/ssmd/es/ESIndexService.java
+++ spring-mybatis-01/src/main/java/com/biantech/ssmd/es/ESIndexService.java
@@ -1,4 +1,41 @@
 package com.biantech.ssmd.es;
 
-public class ESIndexService {
+public class ESIndexBuilder {
+    private Logger logger = LoggerFactory.getLogger(ESIndexBuilder.class);
+    @Autowired
+    private DefaultESRepository defaultESRepository;
+
+    @Autowired
+    private ESMappingBuilder esMappingBuilder;
+
+    public ESResponse createRankIndex(String indexName, String indexCatalog) {
+        ESResponse esResponse = new ESResponse();
+        try {
+            if (ESConstants.IndexCatalog_V1.equalsIgnoreCase(indexCatalog) || StringUtils.isBlank(indexCatalog)) {
+                XContentBuilder xContentBuilder = esMappingBuilder.buildRankScoreMapping();
+                esResponse = defaultESRepository.createIndexMapping(indexName, null, xContentBuilder);
+            }
+        } catch (Exception ex) {
+            logger.warn("createIndexFailed", ex);
+            esResponse.setSucceeded(false);
+            //esResponse.setCause(ex);
+        }
+        return esResponse;
+    }
+
+    public ESResponse createRankIndexDetail(String indexNameDetail, String indexCatalog) {
+        ESResponse esResponse = new ESResponse();
+        try {
+            if (ESConstants.IndexCatalog_V1.equalsIgnoreCase(indexCatalog) || StringUtils.isBlank(indexCatalog)) {
+                XContentBuilder xContentBuilderDetail = esMappingBuilder.buildRankScoreDetailMapping();
+                esResponse = defaultESRepository.createIndexMapping(indexNameDetail, null, xContentBuilderDetail);
+            }
+        } catch (Exception ex) {
+            logger.warn("createIndexFailed", ex);
+            esResponse.setSucceeded(false);
+            //esResponse.setCause(ex);
+        }
+        return esResponse;
+    }
+
 }
