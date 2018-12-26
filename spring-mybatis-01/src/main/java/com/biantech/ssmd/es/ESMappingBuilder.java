9ceb04f7fbf906a5276514114dfe0e0f085e44b0
Jonathan Bian <biantech@163.com>
2018-12-26 09:57:45 +0800

    Update ESMappingBuilder.java

--- spring-mybatis-01/src/main/java/com/biantech/ssmd/es/ESMappingBuilder.java
+++ spring-mybatis-01/src/main/java/com/biantech/ssmd/es/ESMappingBuilder.java
@@ -62,4 +62,30 @@ public class ESMappingBuilder {
         builder.field(FIELD_TYPE,ESConstants.TYPE_VALUE_KEYWORD);
         builder.endObject();
     }
+   
+       private void buildUserAddressInternal(XContentBuilder builder) throws IOException {
+        //builder.startObject("id");
+        //builder.field(FIELD_TYPE, ESConstants.TYPE_VALUE_KEYWORD);
+        //builder.endObject();
+        builder.startObject("user_id");
+        builder.field(FIELD_TYPE, ESConstants.FIELD_TYPE_INT);
+        builder.endObject();
+        builder.startObject("user_name");
+        builder.field(FIELD_TYPE, ESConstants.TYPE_VALUE_KEYWORD);
+        builder.endObject();
+        builder.startObject("address1");
+        builder.field(FIELD_TYPE, ESConstants.FIELD_TYPE_TEXT);
+        builder.field(ESConstants.FIELD_INDEX_ANALYZER,"ik_max_word");
+        builder.field(ESConstants.FIELD_SEARCH_ANALYZER,"ik_max_word");
+        builder.endObject();
+
+        builder.startObject("score");
+        builder.field(FIELD_TYPE, ESConstants.FIELD_TYPE_DOUBLE);
+        builder.endObject();
+        builder.startObject("expireTime");
+        builder.field(FIELD_TYPE, ESConstants.FIELD_TYPE_DATE);
+        //builder.field(FIELD_FORMAT, "yyyy-MM-dd HH:mm:ss");
+        builder.field(FIELD_FORMAT,"date_hour_minute_second");
+        builder.endObject();
+    }
 }
